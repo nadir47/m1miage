e1d2d5f514bb504af63071aef8f98862330452b6
ylgn <yannis.belkebir@etu.parisdescartes.fr>
2020-02-21 00:35:35 +0100

    Adding java doc & test for features_1

--- M1_JAVA_MIAGE/src/main/java/IO_handling_utils/JsonExtractors.java
+++ M1_JAVA_MIAGE/src/main/java/IO_handling_utils/JsonExtractors.java
@@ -7,27 +7,69 @@ import meta_data.Column;
 /**
  * 
  * Contains all functions that help us to extract String from JSon Object
+ * @version 1.0
+ * @author Nadir Omega
+ * @author ylgn
  *
  */
 public class JsonExtractors {
+
+	/**
+	 * Return a data-less Column with a specified rule name and its type.
+	 * The method create an empty column prior calling extractNameAndTypeFromJsObj
+	 * @param  JSONObject giving an object rule
+	 * @param  String giving the ruleName
+	 * @return the column with the specified rule bound to a type
+	 */
 	public static Column initCol(JSONObject jsonObject,String ruleName) 
 	{
 		Column tempCOl = new Column();
 		extractNameAndTypeFromJsObj(tempCOl,jsonObject,ruleName);
 		return tempCOl;	
 	}
+	
+	
+	/**
+	 * Set the adequate rule to a column. 
+	 * It call extractVerificationRuleFromJsObject for dropping dependences 
+	 * between our algorithm and our extract Verification policy.
+	 * @param  Column giving the column that will receive the rule 
+	 * @param  JSONObject giving the object rule
+	 * @param  String giving the rule Name
+	 */
 	public static void parseRuleObject(Column tempCOl,JSONObject jsonObject,String ruleName) 
 	{
 		extractVerificationRuleFromJsObject(tempCOl,jsonObject,ruleName);	
-	}
-	//
-
+	}	
+	
+	/**
+	 * Return the value of an object called name in a JSONObject
+	 * @param  JSONObject giving the object name
+	 * @return String with the value of name
+	 */
 	public static String extractNameFromJsObject(JSONObject jsonObject) {
 		return (String) jsonObject.get("name");
 	}
+	
+	/**
+	 * Return the value of any specified object
+	 * @param  String 
+	 * @param  JSONObject giving an object
+	 * @return String with the value of the asked object
+	 */
 	public static String extractByNameFromJsObject(String str,JSONObject jsonObject) {
 		return (String) jsonObject.get(str);
 	}
+	
+	/**
+	 * Parse one or n rule(s) to a given Column
+	 * If the Json Object has for value a JSONArray, each item will be added to the Rules Array of the given colum
+	 * Otherwise, the rule will be directly mapped.
+	 * @param Column that will receive the rule 
+	 * @param JsonObject carrying the rule(s) 
+	 * @param String giving the targeted rule
+	 * 
+	 */
 	public static void extractVerificationRuleFromJsObject(Column tempCOl,JSONObject jsonObject,String ruleName) {
 		Object ruleObject = jsonObject.get(ruleName);
 		String ruleType = ruleObject.getClass()
@@ -43,15 +85,27 @@ public class JsonExtractors {
 			tempCOl.setRule(ruleObject.toString());
 		}
 	}
+	
+	
+	/**
+	 * Return the value of an anonymised rule given by its name
+	 * @param  JSONObject giving the object rule
+	 * @param  String giving the rule Name
+	 * @return String with the value of the asked object
+	 */
 	public static String extractAnonymisationRuleFromJsObject(JSONObject jsonObject,String ruleName) {
-		//Get employee last name
 		Object ruleObject = jsonObject.get(ruleName);
 		return ruleObject.toString();
 	}
 	
+	/**
+	 * Set the type of a specified rule into a column 
+	 * @param  Column given to be modified
+	 * @param  JSONObject giving the object rule
+	 * @param  String giving the rule Name
+	 */
 	public static void extractNameAndTypeFromJsObj(Column tempCOl,JSONObject jsonObject,String ruleName) {  
 		tempCOl.setName(extractNameFromJsObject(jsonObject));
-		//Get employee last name
 		String type = (String) jsonObject.get(ruleName);
 		tempCOl.setType(type);
 	}
--- M1_JAVA_MIAGE/src/main/java/IO_handling_utils/StringUtils.java
+++ M1_JAVA_MIAGE/src/main/java/IO_handling_utils/StringUtils.java
@@ -1,6 +1,23 @@
 package IO_handling_utils;
 
+/**
+ * 
+ * Contains functions related to String manipulations
+ * @version 1.0
+ * @author Nadir Omega
+ * @author ylgn
+ *
+ */
 public class StringUtils {
+	
+	/**
+	 * Return a data String delimited with a a given delimiter instead of a String tab.
+	 * For each String within the tab, the method add the given delimiter between two objects.
+	 * This is the best thing you need to create CSV with your personal delimiter. 
+	 * @param  String[] giving the data
+	 * @param  String giving the ruleName
+	 * @return A string delimited by the string you asked for
+	 */
 	public static String arrayToStr(String[] data,String joinChar) {
 		StringBuilder str = new StringBuilder("");
 		int size= data.length;
