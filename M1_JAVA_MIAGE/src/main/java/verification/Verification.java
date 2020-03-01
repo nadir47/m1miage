0a453e31d4bc25db98914f05641d9e9d32cab8f7
ylgn <yannis.belkebir@etu.parisdescartes.fr>
2020-03-01 20:14:16 +0100

    Adding test relative to F_60 verification

--- M1_JAVA_MIAGE/src/main/java/verification/Verification.java
+++ M1_JAVA_MIAGE/src/main/java/verification/Verification.java
@@ -3,6 +3,7 @@ package verification;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import Launcher.MainPg;
 import mappers.DescTypeMapper;
 import mappers.VerificationRuleMapper;
